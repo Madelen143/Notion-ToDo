cb0ce79e99d7cedca382e5e8f412b49a32b21d62
Madelen <madelenf143@berkeley.edu>
2024-07-04 14:07:16 -0700

    resolved 401 error by installing dotenv to access env vars

--- ToDo.js
+++ ToDo.js
@@ -1,9 +1,12 @@
-// Node.js needed if running back-end (serve side) application
+//! Node.js needed if running back-end (serve side) application
+// allows us to access environment variables
+require("dotenv").config();
+
 // require() is how Node.js loads modules
 const { Client } = require('@notionhq/client');
 
 
-const notion = new Client({ auth: process.env.NOTION_API_KEY });
+const notion = new Client({ auth: process.env.NOTION_KEY });
 
 (async () => {
   const databaseId = process.env.NOTION_PAGE_ID;
