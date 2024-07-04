3a58e2f1d026e9105ecaab84df25ef02e1bca85d
Madelen <madelenf143@berkeley.edu>
2024-07-04 13:37:54 -0700

    added package.json file and added dependencies

---
+++ ToDo.js
@@ -0,0 +1,13 @@
+// Node.js needed if running back-end (serve side) application
+// require() is how Node.js loads modules
+const { Client } = require('@notionhq/client');
+
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+(async () => {
+  const databaseId = process.env.NOTION_PAGE_ID;
+  const response = await notion.databases.retrieve({ database_id: databaseId });
+  console.log(response);
+})();
+
